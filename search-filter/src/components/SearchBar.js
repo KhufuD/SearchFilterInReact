53b2bfc3a6e2ca5aac2556d3972167a53f3c1053
khufu <khufudev@gmail.com>
2022-06-15 12:52:46 -0700

    Empty CSS files added and Form and list components made

---
+++ search-filter/src/components/SearchBar.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const SearchBar = () => {
+    return (
+        <form>
+            <label>Filter here</label>
+            <input></input>
+        </form>
+    );
+};
+
+export default SearchBar;
